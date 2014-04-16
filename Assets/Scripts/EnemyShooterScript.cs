d8c1ce51d986f7f246b90ade060160b45bf22030
Brian Kha <brian.kha@asu.edu>
2014-04-16 05:46:16 -0700

    added shooter animations

--- Assets/Scripts/EnemyShooterScript.cs
+++ Assets/Scripts/EnemyShooterScript.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class EnemyShooterScript : MonoBehaviour {
 
     private WeaponScript weapon;
+    private bool shooting = true;
+    private Animator animator;
 
     void Awake()
     {
         weapon = GetComponentInChildren<WeaponScript>();
+        animator = GetComponent<Animator>();
     }
 
 	// Use this for initialization
@@ -17,10 +20,17 @@ public class EnemyShooterScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        weapon.shootingRate = .1f;
 
-        if (weapon != null && weapon.enabled && weapon.CanAttack())
-        {
-            weapon.Attack(true,0,5);
+        animator.SetBool("shooting", shooting);
+
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Enemy_Shooter_Contracted"))
+        {//this.animator.GetCurrentAnimatorStateInfo(0).IsName("YourAnimationName"))
+            if (shooting && weapon != null && weapon.enabled && weapon.CanAttack())
+            {
+                weapon.Attack(true, 0, 5);
+                animator.SetTrigger("shoot");
+            }
         }
 	}
 }
