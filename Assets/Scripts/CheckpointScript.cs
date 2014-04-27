1dd3f59ab18ab474e5ccc4c6acaad558965bff42
Brian Kha <brian.kha@asu.edu>
2014-04-27 04:50:21 -0700

    started working on checkpointscript

---
+++ Assets/Scripts/CheckpointScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckpointScript : MonoBehaviour {
+
+    private bool isActive = false;
+    private RotateScript[] rotates;
+
+    void Awake()
+    {
+        rotates = GetComponentsInChildren<RotateScript>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
